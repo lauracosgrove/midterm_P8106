b5b46282abd2c1a8a6c831258169f3a58fb533f4
Alyssa <amv2187@cumc.columbia.edu>
2019-03-26 15:37:10 -0400

    test and train datasets

---
+++ test_train_split.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(readr)
+
+## Data import
+data <- read_csv("data/Training_values.csv")
+
+
+## Manipulation
+data <- data %>%
+  separate(col = area__rucc, into = c('metro', 'population'), sep = ' - ')
+
+
+## training/test data
+set.seed(1)
+train_ind <- sample(seq_len(nrow(data)), size = 2/3*nrow(data))
+
+train <- data[train_ind, ]
+test <- data[-train_ind, ]
+
+# write.csv(train, file = './data/train.csv', row.names = F)
+# write.csv(test, file = './data/test.csv', row.names = F)
